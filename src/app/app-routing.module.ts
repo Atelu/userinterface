357d861a5888d034d7832a97ab6bcfc22f841ec5
Atelu <vedem98@yahoo.com>
2019-07-02 04:01:10 +0000

    git commit -m "First commit"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+  },
+
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginModule',
+  },
+
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
